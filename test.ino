ec25c13f12f0f9418c6259ca69b1b045d80f5c0e
Phillip Kessels <info@phikes.de>
2014-09-27 16:46:58 +0200

    Restricted to one octave

--- test.ino
+++ test.ino
@@ -1,34 +1,29 @@
 #include <MIDI.h>
 #include <NewPing.h>
 
-#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 100 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-// Simple tutorial on how to receive and send MIDI messages.
-// Here, when receiving any message on channel 4, the Arduino
-// will blink a led and play back a note for 1 second.
-MIDI_CREATE_DEFAULT_INSTANCE();
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+#define TRIGGER_PIN  12  
+#define ECHO_PIN     11  
+#define MAX_DISTANCE 30
 
-#define LED 13   		    // LED pin on Arduino Uno
+MIDI_CREATE_DEFAULT_INSTANCE();
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup()
 {
-    //MIDI.begin(4);          // Launch MIDI and listen to channel 4
     Serial.begin(115200);
 }
 
 void loop()
 {
 
-        unsigned int dist = sonar.ping() / US_ROUNDTRIP_CM;
+        unsigned int dist = ((float)sonar.ping()) / US_ROUNDTRIP_CM;
         float ratio = (MAX_DISTANCE-(float)dist)/MAX_DISTANCE;
-        int note = 127*ratio;
+        int note = floor(59+8*ratio);
         
-        if(note<=123)
+        if(note<67)
         {
-          MIDI.sendNoteOn(note,127,1);
+          MIDI.sendNoteOn(note, 127, 1);
           delay(50);        
-          MIDI.sendNoteOff(note,127,1);
+          MIDI.sendNoteOff(note, 127, 1);
         }
 }
